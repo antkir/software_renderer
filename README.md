fd6e6be8c7fea2f3f110488a4e294ae56586a164
Kiryl Antonik <antkirp@gmail.com>
2019-11-15 00:25:08 +0300

    fix VS build

--- README.md
+++ README.md
@@ -2,4 +2,25 @@
 
 Just a basic software renderer. Using SDL2 for creating windows.
 
+## Build
+
+*Windows*
+
+```
+mkdir build
+cd build
+cmake .. -G "Visual Studio 16 2019"
+```
+
+This will generate the .sln project in the `build` folder, which you can open in Visual Studio.
+
+*Unix*
+
+```
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make
+```
+## Demo
 [Demo video](https://giant.gfycat.com/SpitefulTinyFoal.webm)
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "renderer.h"
 
 #include <iostream>
+#include <ghc/filesystem.hpp>
 #include <SDL2/SDL.h>
 
 constexpr uint16_t DEFAULT_WIDTH = 800;
@@ -44,12 +45,12 @@ int main(int argc, char* argv[]) {
     SDL_Surface* surface = SDL_GetWindowSurface(window.get());
     auto* pixels = static_cast<uint32_t*>(surface->pixels);
 
-    std::unique_ptr<renderer::Renderer> renderer =
-            std::make_unique<renderer::Renderer>(window.get(), DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_CLEAR_COLOR);
+    renderer::Renderer renderer = renderer::Renderer(window.get(), DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_CLEAR_COLOR);
 
     std::unique_ptr<renderer::Model> model;
     try {
-        model = std::make_unique<renderer::Model>("data/Pallas_Cat");
+        ghc::filesystem::path model_path = ghc::filesystem::path(argv[0]).parent_path() / "data" / "Pallas_Cat";
+        model = std::make_unique<renderer::Model>(model_path.string());
     } catch (const std::runtime_error& error) {
         std::cout << "Runtime Error: " << error.what() << std::endl;
         return 1;
@@ -67,7 +68,7 @@ int main(int argc, char* argv[]) {
                         case SDL_WINDOWEVENT_RESIZED:
                             surface = SDL_GetWindowSurface(window.get());
                             pixels = static_cast<uint32_t*>(surface->pixels);
-                            renderer->resize_window(event.window.data1, event.window.data2);
+                            renderer.resize_window(event.window.data1, event.window.data2);
                             break;
                         default:
                             break;
@@ -81,14 +82,14 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        renderer->clear_buffer(pixels);
+        renderer.clear_buffer(pixels);
 
         // Draw model
         math::Matrix<4, 4> rotation_mtx1 = math::create_rotation_matrix(1.f, 0.f, 0.f, 1.6f);
         math::Matrix<4, 4> rotation_mtx2 = math::create_rotation_matrix(0.f, 0.f, 1.f, current_tick / 5000.f);
         math::Matrix<4, 4> rotation_mtx = math::mul(rotation_mtx1, rotation_mtx2);
         math::Matrix<4, 4> translation_mtx = math::create_translation_matrix(1.f, 15.f, 50.f);
-        renderer->draw_model(model.get(), pixels, rotation_mtx, translation_mtx, 60.f);
+        renderer.draw_model(model.get(), pixels, rotation_mtx, translation_mtx, 60.f);
 
         // FPS
         delta_ticks = SDL_GetTicks() - current_tick;
